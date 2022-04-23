94117109def9ab991b554cd4f32349a90078841a
StephenAraka <stevenaraka@gmail.com>
2022-04-23 14:25:55 +0200

    ts basics

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,88 @@
 "use strict";
+//- Basic primitive types
 let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+let age;
+//- Arrays
+let ids = [1, 2, 3, 4];
+let arr; // use any when you dont know what type will be innit
+//- Tuple
+let person = [1, 'Brad', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill']
+];
+//- Union - variable hold more than 1 type
+let prodID;
+prodID = '22';
+prodID = 33;
+//- Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+//- Objects
+const user = {
+    id: 1,
+    name: 'Steve'
+};
+const user2 = {
+    id: 1,
+    name: 'Steve'
+};
+//- Type Assertion
+// <type> or 'as type'
+let cid = 1;
+// let customerID = <number>cid // or
+let customerID = cid;
+//- Functions
+function addNum(x, y) {
+    return x + y;
+}
+// when you dont have a return value: void
+function log(message) {
+    console.log(message);
+}
+log('true');
+const user3 = {
+    id: 1,
+    name: 'Steve'
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+//- Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, 'Brad');
+const steve = new Person(2, 'Steve');
+// console.log(brad, steve)
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const employee1 = new Employee(3, 'Chris', 'Dev');
+console.log(employee1.register());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,157 @@
+//- Basic primitive types
 let id: number = 5;
+let company: string = 'Traversy Media';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+let age: number;
+
+//- Arrays
+let ids: number[] = [1, 2, 3, 4];
+let arr: any[]; // use any when you dont know what type will be innit
+
+//- Tuple
+let person: [number, string, boolean] = [1, 'Brad', true];
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill']
+];
+
+//- Union - variable hold more than 1 type
+let prodID: string | number;
+prodID = '22';
+prodID = 33;
+
+//- Enum
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right'
+}
+
+//- Objects
+const user: {
+  id: number,
+  name: string
+} = {
+  id: 1,
+  name: 'Steve'
+}
+
+// Objects - types
+type User = {
+  id: number,
+  name: string
+}
+
+const user2: User = {
+  id: 1,
+  name: 'Steve'
+}
+
+//- Type Assertion
+// <type> or 'as type'
+let cid: any = 1
+// let customerID = <number>cid // or
+let customerID = cid as number
+
+//- Functions
+function addNum(x: number, y: number): number {
+  return x + y
+}
+
+// when you dont have a return value: void
+function log(message: string | number): void {
+  console.log(message)
+}
+
+log('true')
+
+
+//- Interface
+// Use with objects - cant use unions with interfaces
+interface UserInterface {
+  id: number,
+  name: string
+}
+
+const user3: UserInterface = {
+  id: 1,
+  name: 'Steve'
+}
+
+// More Properties
+interface UserInterface2 {
+  readonly id: number,      // readonly
+  name: string,
+  age?: number              // optional
+}
+
+// Interface with functions
+interface MathFunc {
+  (x: number, y: number): number
+}
+
+const add: MathFunc = (x, y) => x + y;
+const sub: MathFunc = (x, y) => x - y;
+
+interface PersonInterface {
+  id: number
+  name: string
+  register(): string
+}
+
+//- Classes
+class Person implements PersonInterface {
+  id: number
+  name: string
+
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
+
+  register(): string {
+    return `${this.name} is now registered`
+  }
+}
+
+const brad = new Person(1, 'Brad')
+const steve = new Person(2, 'Steve')
+
+
+// console.log(brad, steve)
+
+// Sub-classes - extending a parent class
+class Employee extends Person {
+  position: string
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name)
+    this.position = position
+  }
+}
+
+const employee1 = new Employee(3, 'Chris', 'Dev')
+
+console.log(employee1.register())
+
+//- Generics
+// useful for creating reusable functions
+function getArray<T>(items: T[]): T[] {   // T = typePlaceholder
+  return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['one', 'two', 'three'])
